e46f9541535028e68199968b6a2726006a341491
Jeff Zhong <jeff@Jeffs-MacBook-Air.local>
2023-11-10 14:16:45 -0800

    add to gitignore

--- pages/index.js
+++ pages/index.js
@@ -7,26 +7,24 @@ import dynamic from 'next/dynamic';
 const SurveyComponent = dynamic(() => import("./SurveyComponent"), {ssr: false})
  
 export default function Home() {
-  // if (typeof window !== "undefined") {
-    return (
-      <div>
-        <Head>
-          <link rel="icon" href="/favicon.ico" sizes="any"/>  
-        </Head>
-        <main className={styles.main}>
-            <div>
-              <img id="fuser_now_logo" src="fuser_now_logo.png" alt="FuserNow Logo" width="100%"></img>
-              <SurveyComponent></SurveyComponent>
-            </div>
-        </main> 
-        <footer className={styles.footer}>
-          <div className={styles.copyright}>
-            &copy; 2023 FuserNow. All rights reserved.
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any"/>  
+      </Head>
+      <main className={styles.main}>
+          <div>
+            <img id="fuser_now_logo" src="fuser_now_logo.png" alt="FuserNow Logo" width="50%" height="auto" text_align="center"></img>
+            <SurveyComponent></SurveyComponent>
           </div>
-        </footer>
-      </div>
-    )
-  // }
+      </main> 
+      <footer className={styles.footer}>
+        <div className={styles.copyright}>
+          &copy; 2023 FuserNow. All rights reserved.
+        </div>
+      </footer>
+    </div>
+  )
 }
 
 
