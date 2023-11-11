90707115321d87c48b1172481508c72342dd5ca3
Jeff Zhong <jeff@Jeffs-MacBook-Air.local>
2023-11-10 16:53:27 -0800

    done

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,30 @@ import React from "react";
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 
-const SurveyComponent = dynamic(() => import("./SurveyComponent"), {ssr: false})
+const SurveyComponent = dynamic(() => import("../public/SurveyComponent"), {ssr: false})
  
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any"/>  
-      </Head>
-      <main className={styles.main}>
-          <div>
-            <img id="fuser_now_logo" src="fuser_now_logo.png" alt="FuserNow Logo" width="50%" height="auto" text_align="center"></img>
-            <SurveyComponent></SurveyComponent>
+  if (typeof window !== undefined) {
+    return (
+      <div>
+        <Head>
+          <title>FuserNow Rewards</title>
+          <link rel="icon" href="/favicon.ico" sizes="any"/>  
+        </Head>
+        <main className={styles.main}>
+            <div>
+              <img id="fuser_now_logo" src="fuser_now_logo.png" alt="FuserNow Logo" width="50%" height="auto" text_align="center"></img>
+              <SurveyComponent></SurveyComponent>
+            </div>
+        </main> 
+        <footer className={styles.footer}>
+          <div className={styles.copyright}>
+            &copy; 2023 FuserNow. All rights reserved.
           </div>
-      </main> 
-      <footer className={styles.footer}>
-        <div className={styles.copyright}>
-          &copy; 2023 FuserNow. All rights reserved.
-        </div>
-      </footer>
-    </div>
-  )
+        </footer>
+      </div>
+    )
+  }
 }
 
 
